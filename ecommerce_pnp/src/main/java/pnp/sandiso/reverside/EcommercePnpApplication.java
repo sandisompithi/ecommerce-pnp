cb043f144e29132714ebcba7973890d3dbdf292d
sandisompithi <sandisompithi@outlook.com>
2020-06-15 15:30:09 +0200

    Initial commit

---
+++ ecommerce_pnp/src/main/java/pnp/sandiso/reverside/EcommercePnpApplication.java
@@ -0,0 +1,13 @@
+package pnp.sandiso.reverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePnpApplication.class, args);
+	}
+
+}
---
+++ ecommerce_pnp/src/test/java/pnp/sandiso/reverside/EcommercePnpApplicationTests.java
@@ -0,0 +1,13 @@
+package pnp.sandiso.reverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
