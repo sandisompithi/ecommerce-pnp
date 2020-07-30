0590e3a02ec2df661a850d6de2455cee8a82d652
sandisompithi <sandisompithi@outlook.com>
2020-07-30 13:16:55 +0200

    add a bean for CORS Mapping

--- ecommerce_pnp/src/main/java/pnp/sandiso/reverside/config/WebSecurityConfig.java
+++ ecommerce_pnp/src/main/java/pnp/sandiso/reverside/config/WebSecurityConfig.java
@@ -14,6 +14,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pnp.sandiso.reverside.config.jwt.AuthEntryPointJwt;
 import pnp.sandiso.reverside.config.jwt.AuthTokenFilter;
 import pnp.sandiso.reverside.config.services.UserDetailsServiceImpl;
@@ -60,5 +66,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			.anyRequest().authenticated();
 		
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsCnfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/home/**")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
 	}
 }
